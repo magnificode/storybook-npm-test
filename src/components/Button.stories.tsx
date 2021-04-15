3d8409ed1e05251299868725246748b977e8d1fc
Dominic Magnifico <dom@houseofgiants.com>
2021-04-15 08:50:34 -0600

    Move Button story.

--- src/components/Button.stories.tsx
+++ src/components/Button.stories.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Story, Meta } from '@storybook/react';
 
-import { Button, ButtonProps } from '../components/Button';
+import { Button, ButtonProps } from './Button';
 
 export default {
 	title: 'Example/Button',
