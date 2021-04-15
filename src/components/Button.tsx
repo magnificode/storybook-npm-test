68c2e11f5963c97803d9ae979d0f9ffc8d117534
Dominic Magnifico <dom@houseofgiants.com>
2021-04-15 09:10:06 -0600

    Add styled component styles.

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './button.css';
+import styled from 'styled-components';
 
 export interface ButtonProps {
 	/**
@@ -16,21 +16,58 @@ export interface ButtonProps {
 	onClick?: () => void;
 }
 
+export const StyledButton = styled.button<ButtonProps>`
+	font-family: 'Nunito Sans', 'Helvetica Neue', Helvetica, Arial, sans-serif;
+	font-weight: 700;
+	border: 0;
+	border-radius: 3em;
+	cursor: pointer;
+	display: inline-block;
+	line-height: 1;
+
+	&.storybook-button--primary {
+		color: white;
+		background-color: #1ea7fd;
+	}
+
+	&.storybook-button--secondary {
+		background-color: transparent;
+		box-shadow: rgba(0, 0, 0, 0.15) 0px 0px 0px 1px inset;
+		color: #333;
+	}
+
+	&.storybook-button--small {
+		font-size: 12px;
+		padding: 10px 16px;
+	}
+
+	&.storybook-button--medium {
+		font-size: 14px;
+		padding: 11px 20px;
+	}
+
+	&.storybook-button--large {
+		font-size: 16px;
+		padding: 12px 24px;
+	}
+`
+
 /**
  * Primary UI component for user interaction
  */
 export const Button: React.FC<ButtonProps> = ({
 	children,
+	variant,
 	size = 'medium',
 	...props
 }) => {
 	return (
-		<button
+		<StyledButton
 			type="button"
-			className={['storybook-button', `storybook-button--${size}`].join(' ')}
+			className={['storybook-button', `storybook-button--${size}`, `storybook-button--${variant?.toLowerCase()}`].join(' ')}
 			{...props}
 		>
 			{children}
-		</button>
+		</StyledButton>
 	);
 };
