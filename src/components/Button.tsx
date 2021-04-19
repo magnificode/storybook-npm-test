959c7898897b1d31ce957ee9d255fc115802a8da
Dominic Magnifico <dom@houseofgiants.com>
2021-04-19 17:24:12 -0600

    Convert px to rem.

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,73 +1,77 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
 export interface ButtonProps {
-	/**
-	 * The Button variant
-	 */
-	variant?: 'Primary' | 'Secondary' | 'Tertiary';
-	/**
-	 * How large should the button be?
-	 */
-	size?: 'small' | 'medium' | 'large';
-	/**
-	 * Optional click handler
-	 */
-	onClick?: () => void;
+  /**
+   * The Button variant
+   */
+  variant?: "Primary" | "Secondary" | "Tertiary";
+  /**
+   * How large should the button be?
+   */
+  size?: "small" | "medium" | "large";
+  /**
+   * Optional click handler
+   */
+  onClick?: () => void;
 }
 
 export const StyledButton = styled.button`
-	font-family: 'Nunito Sans', 'Helvetica Neue', Helvetica, Arial, sans-serif;
-	font-weight: 700;
-	border: 0;
-	border-radius: 3em;
-	cursor: pointer;
-	display: inline-block;
-	line-height: 1;
+  font-family: "Nunito Sans", "Helvetica Neue", Helvetica, Arial, sans-serif;
+  font-weight: 700;
+  border: 0;
+  border-radius: 3em;
+  cursor: pointer;
+  display: inline-block;
+  line-height: 1;
 
-	&.storybook-button--primary {
-		color: white;
-		background-color: #1ea7fd;
-	}
+  &.-primary {
+    color: #333;
+    background-color: #98f5c9;
+  }
 
-	&.storybook-button--secondary {
-		background-color: transparent;
-		box-shadow: rgba(0, 0, 0, 0.15) 0px 0px 0px 1px inset;
-		color: #333;
-	}
+  &.-secondary {
+    background-color: transparent;
+    box-shadow: rgba(0, 0, 0, 0.15) 0px 0px 0px 1px inset;
+    color: #333;
+  }
 
-	&.storybook-button--small {
-		font-size: 12px;
-		padding: 10px 16px;
-	}
+  &.-small {
+    font-size: 0.75rem;
+    padding: 0.625rem 1rem;
+  }
 
-	&.storybook-button--medium {
-		font-size: 14px;
-		padding: 11px 20px;
-	}
+  &.-medium {
+    font-size: 0.875rem;
+    padding: 0.625rem 1.25rem;
+  }
 
-	&.storybook-button--large {
-		font-size: 16px;
-		padding: 12px 24px;
-	}
-`
+  &.-large {
+    font-size: 1rem;
+    padding: 0.75rem 1.5rem;
+  }
+`;
 
 /**
  * Primary UI component for user interaction
  */
 export const Button: React.FC<ButtonProps> = ({
-	children,
-	variant = 'primary',
-	size = 'medium',
-	...props
+  children,
+  variant = "primary",
+  size = "medium",
+  ...props
 }) => {
-	return (
-		<StyledButton
-			type="button"
-			className={['storybook-button', `storybook-button--${size}`, `storybook-button--${variant?.toLowerCase()}`].join(' ')}
-			{...props}
-		>
-			{children}
-		</StyledButton>
-	);
+  return (
+    <StyledButton
+      type="button"
+      className={[
+        "storybook-button",
+        `-${size}`,
+        `-${variant?.toLowerCase()}`,
+      ].join(" ")}
+      {...props}
+    >
+      {children}
+    </StyledButton>
+  );
 };
