f53f528b216660e389fcfa6b85e65d6e4328a386
Dominic Magnifico <dom@houseofgiants.com>
2021-04-21 09:20:44 -0600

    Add button component.

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+import { StyledButton } from "./StyledButton";
+
+export interface ButtonProps {
+  /**
+   * The Button variant
+   */
+  variant?: "Primary" | "Secondary" | "Tertiary";
+  /**
+   * How large should the button be?
+   */
+  size?: "small" | "medium" | "large";
+  /**
+   * Optional click handler
+   */
+  onClick?: () => void;
+}
+
+/**
+ * Primary UI component for user interaction
+ */
+export const Button: React.FC<ButtonProps> = ({ children, variant = "primary", size = "medium", ...props }) => (
+  <StyledButton type="button" className={["storybook-button", `-${size}`, `-${variant?.toLowerCase()}`].join(" ")} {...props}>
+    {children}
+  </StyledButton>
+);
